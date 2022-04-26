44301f159019073b9abd3fbd30648280e3c88d4b
ShadowFever3 <90917040+ShadowFever3@users.noreply.github.com>
2022-04-26 13:10:10 -0400

    Working arrow + rand tp
    An added arrow shot that now goes toward the player.
    
    I am still missing that it rotates toward the player.
    
    Random Tp was added

--- Assets/Chiron - Boss Room/Script/bossChiron.cs
+++ Assets/Chiron - Boss Room/Script/bossChiron.cs
@@ -14,6 +14,11 @@ public class bossChiron : MonoBehaviour
     int arrowSpeed;
     [SerializeField]
     GameObject player;
+
+    [SerializeField]
+    Transform teleportTarget_TL, teleportTarget_TR, teleportTarget_ML, teleportTarget_MR, teleportTarget_BL, teleportTarget_BR;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,21 +28,82 @@ public class bossChiron : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Shot arrow
         if (Input.GetKeyDown(KeyCode.P))
         {
             shotArrow();
         }
+
+        float randTp = UnityEngine.Random.Range(0, 2000);
+        Debug.Log("Rand Value: " + randTp);
+        //Teleport Chiron
+        if (Input.GetKeyDown(KeyCode.T) || randTp == 1000f)
+        {
+            
+            tpRand();
+        }
     }
+
+    private void tpRand()
+    {
+        float rand = UnityEngine.Random.Range(0,6);
+        switch (rand)
+        {
+            case 0:
+                gameObject.transform.position = teleportTarget_BL.transform.position;
+                gameObject.transform.localScale = new Vector3(-1, 1, 0);
+                break;
+            case 1:
+                gameObject.transform.position = teleportTarget_ML.transform.position;
+                gameObject.transform.localScale = new Vector3(-1, 1, 0);
+                break;
+            case 2:
+                gameObject.transform.position = teleportTarget_TL.transform.position;
+                gameObject.transform.localScale = new Vector3(-1, 1, 0);
+                break;
+            case 3:
+                gameObject.transform.position = teleportTarget_BR.transform.position;
+                gameObject.transform.localScale = new Vector3(1, 1, 0);
+                break;
+            case 4:
+                gameObject.transform.position = teleportTarget_MR.transform.position;
+                gameObject.transform.localScale = new Vector3(1, 1, 0);
+                break;
+            case 5:
+                gameObject.transform.position = teleportTarget_TR.transform.position;
+                gameObject.transform.localScale = new Vector3(1, 1, 0);
+                break;
+        }
+        
+    }
+
+
+
+
+
+
+
+
+
+
 
     private void shotArrow()
     {
+        //Spawn arrow
         GameObject newArrow = Instantiate(arrow, arrowPosition.position, arrowPosition.rotation);
+
+        //Rotate arrow toward the player
+        //Vector3 currentDirection = arrowPosition.forward;
+        //Vector3 playerDirection = player.transform.position - arrowPosition.position;
+        //arrowPosition.forward = Vector3.RotateTowards(currentDirection, playerDirection, speed * 100, 180.0f);
+
+        //Shot to player
         Rigidbody2D rb2d = newArrow.GetComponent<Rigidbody2D>();
+        rb2d.velocity = (player.transform.position - newArrow.transform.position).normalized * arrowSpeed;
 
-        Vector2 direction = gameObject.transform.position - player.transform.position;
-        Vector2 newvector = direction * speed;
-        rb2d.velocity = newvector;
 
-        //rb2d.AddForce((player.GetComponent<Rigidbody2D>().position - newArrow.GetComponent<Rigidbody2D>().position) * arrowSpeed);
+        Destroy(newArrow, 3.0f);
+
+
     }
 }
