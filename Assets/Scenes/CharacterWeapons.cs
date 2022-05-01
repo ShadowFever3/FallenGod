9a917309446da421906217e1042a4300b3bcbc7c
CptDoritos <fred.vezina@hotmail.com>
2022-05-01 16:13:50 -0400

    Commit3Fred20220501

--- Assets/Scenes/CharacterWeapons.cs
+++ Assets/Scenes/CharacterWeapons.cs
@@ -8,6 +8,8 @@ public class CharacterWeapons : MonoBehaviour
     [SerializeField]
     GameObject Character, projectile1, projectile2, projectile3, projectile4, secretprojectile, melee1, melee2;
     GameObject c, projectile, melee;
+    [SerializeField]
+    Quaternion bulletrotation;
 
     bool ready, right, left, inair;
 
@@ -73,6 +75,89 @@ public class CharacterWeapons : MonoBehaviour
             }
         }
 
+        if(Input.GetKey(KeyCode.Mouse0))
+        {
+            if(ready == true)
+            {
+            if(projectile == projectile4 || projectile == secretprojectile)
+            {
+                getCount = GameObject.FindGameObjectsWithTag("ShotgunAmmo");
+                count = getCount.Length;
+                if(count < 250){
+                   if(right == true && inair == false)
+            {
+            c = Instantiate(projectile, Character.transform.position+(transform.right*0.15f), Character.transform.rotation);
+            c.GetComponent<Rigidbody2D>().velocity = transform.right * 5;
+            }else if(left == true && inair == false)
+            {
+                c = Instantiate(projectile, Character.transform.position+(-transform.right*0.15f), Character.transform.rotation);
+                c.GetComponent<Rigidbody2D>().velocity = -transform.right * 5;
+            }else if(inair == true)
+            {
+                c = Instantiate(projectile, Character.transform.position+((-transform.up * 0.45f) + (transform.right * 0.3f)), bulletrotation);
+            }
+            Destroy(c, 5f);
+            }else{
+                StartCoroutine(waiter());
+                }
+            }
+            }
+        }
+
+        if(Input.GetMouseButtonDown(1))
+        {
+            if(ready == true)
+            {
+                getCount = GameObject.FindGameObjectsWithTag("Melee");
+                count = getCount.Length;
+                if(count < 1){
+                   if(right == true)
+            {
+            c = Instantiate(melee, Character.transform.position+(transform.right*1.75f), Character.transform.rotation);
+            }else if(left == true)
+            {
+                c = Instantiate(melee, Character.transform.position+(-transform.right*1.75f), Character.transform.rotation);
+                Vector3 theScale = c.transform.localScale;
+                theScale.x *= -1.4f;
+                c.transform.localScale = theScale;
+            }
+            Destroy(c, 0.5f);
+            }else{
+                StartCoroutine(waiter());
+                }
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            if(ready == true)
+            {
+            if(melee == melee1)
+            {
+                getCount = GameObject.FindGameObjectsWithTag("Melee");
+                count = getCount.Length;
+                if(count < 1){
+                   if(right)
+            {
+            c = Instantiate(melee, Character.transform.position+(transform.right*1.55f), Character.transform.rotation);
+            c.GetComponent<Rigidbody2D>().velocity = transform.right * 5;
+
+            }else if(left)
+            {
+                c = Instantiate(melee, Character.transform.position+(-transform.right*1.55f), Character.transform.rotation);
+                c.GetComponent<Rigidbody2D>().velocity = -transform.right * 5;
+                Vector3 theScale = c.transform.localScale;
+                theScale.x *= -1.4f;
+                c.transform.localScale = theScale;
+            }
+            Destroy(c, 2.5f);
+            }else{
+                StartCoroutine(waiter());
+                }
+            }
+            }
+        }     
+
         IEnumerator waiter()
     {
 
@@ -83,4 +168,63 @@ public class CharacterWeapons : MonoBehaviour
         ready = true;
     }
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        switch(collision.gameObject.tag){
+        case "Default Weapon":
+        WeaponSaving.projectileset = 1;
+        break;
+        case "Super Weapon":
+        WeaponSaving.projectileset = 2;
+        break;
+        case "Bomb":
+        WeaponSaving.projectileset = 3;
+        break;
+        case "Shotgun":
+        WeaponSaving.projectileset = 4;
+        break;
+        case "Default Melee":
+        WeaponSaving.meleeset = 1;
+        break;
+        case "Sword":
+        WeaponSaving.meleeset = 2;
+        break;
+        }
+
+        switch(WeaponSaving.projectileset){
+
+            case 1:
+            projectile = projectile1;
+            break;
+
+            case 2:
+            projectile = projectile2;
+            break;
+
+            case 3:
+            projectile = projectile3;
+            break;
+
+            case 4:
+            projectile = projectile4;
+            break;
+
+            case 5:
+            projectile = secretprojectile;
+            break;
+        }
+
+        switch(WeaponSaving.meleeset){
+
+            case 1:
+            melee = melee1;
+            break;
+
+            case 2:
+            melee = melee2;
+            break;
+        }
+    }
+
 }
