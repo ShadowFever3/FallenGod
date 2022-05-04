d95684307d09be6e2dbcd4e91a7aee18e3856c93
CptDoritos <fred.vezina@hotmail.com>
2022-05-03 21:12:27 -0400

    FinalCommitFred20220503

--- Assets/Momo1/ScriptMenuScene/Options.cs
+++ Assets/Momo1/ScriptMenuScene/Options.cs
@@ -10,6 +10,9 @@ public class Options : MonoBehaviour
     [SerializeField]
     Canvas mainmenuCanvas;
 
+    [SerializeField]
+    Canvas ControlsCanvas;
+
 
     [SerializeField]
     GameObject Image;
@@ -17,13 +20,18 @@ public class Options : MonoBehaviour
     [SerializeField]
     GameObject OptionImage;
 
+    [SerializeField]
+    GameObject ControlsImage;
+
 
 
     private void Start()
     {
         optionsCanvas.GetComponent<Canvas>().enabled = false;
+        ControlsCanvas.GetComponent<Canvas>().enabled = false;
         mainmenuCanvas.GetComponent<Canvas>().enabled = true;
         OptionImage.SetActive(false);
+        ControlsImage.SetActive(false);
         Image.SetActive(true);
     }
 
@@ -32,12 +40,22 @@ public class Options : MonoBehaviour
         if (optionsCanvas.GetComponent<Canvas>().enabled == true)
         {
             Image.SetActive(false);
+            ControlsImage.SetActive(false);
             OptionImage.SetActive(true);
 
         }
         if (mainmenuCanvas.GetComponent<Canvas>().enabled == true)
         {
             Image.SetActive(true);
+            ControlsImage.SetActive(false);
+            OptionImage.SetActive(false);
+
+        }
+
+        if (ControlsCanvas.GetComponent<Canvas>().enabled == true)
+        {
+            Image.SetActive(false);
+            ControlsImage.SetActive(true);
             OptionImage.SetActive(false);
 
         }
